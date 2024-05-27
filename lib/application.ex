c44505cfa058b0ee27609479231e132636a6725d
Chris McCord <chris@chrismccord.com>
2024-05-27 12:33:30 -0400

    Debug

--- lib/application.ex
+++ lib/application.ex
@@ -2,6 +2,7 @@ defmodule FLAME.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
@@ -13,6 +14,7 @@ defmodule FLAME.Application do
     children = [
       Supervisor.child_spec({FLAME.Terminator, opts}, shutdown: shutdown)
     ]
+    Logger.info("starting FLAME #{inspect(children)}")
 
     opts = [strategy: :one_for_one, name: FLAME.Supervisor]
     Supervisor.start_link(children, opts)
