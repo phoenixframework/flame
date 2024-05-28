f735071c30f7ad1d37ea61b3bb68f4c3b65595ed
José Valim <jose.valim@dashbit.co>
2024-05-28 21:38:06 +0200

    Make castore optional on the backend

--- lib/flame/fly_backend.ex
+++ lib/flame/fly_backend.ex
@@ -325,12 +325,11 @@ defmodule FLAME.FlyBackend do
     http_opts = [
       ssl: [
         verify: :verify_peer,
-        cacertfile: String.to_charlist(CAStore.file_path()),
         depth: 2,
         customize_hostname_check: [
           match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
         ]
-      ],
+      ] ++ cacerts_options(),
       connect_timeout: connect_timeout
     ]
 
@@ -344,5 +343,45 @@ defmodule FLAME.FlyBackend do
       {:error, reason} ->
         raise "failed POST #{url} with #{inspect(reason)} #{inspect(headers)}"
     end
+  end
+
+  defp cacerts_options do
+    cond do
+      certs = otp_cacerts() ->
+        [cacerts: certs]
+
+      Application.spec(:castore, :vsn) ->
+        [cacertfile: Application.app_dir(:castore, "priv/cacerts.pem")]
+
+      true ->
+        IO.warn("""
+        No certificate trust store was found.
+
+        A certificate trust store is required in
+        order to download locales for your configuration.
+        Since elixir_make could not detect a system
+        installed certificate trust store one of the
+        following actions may be taken:
+
+        1. Use OTP 25+ on an OS that has built-in certificate
+           trust store.
+
+        2. Install the hex package `castore`. It will
+           be automatically detected after recompilation.
+
+        """)
+
+        []
+    end
+  end
+
+  if System.otp_release() >= "25" do
+    defp otp_cacerts do
+      :public_key.cacerts_get()
+    rescue
+      _ -> nil
+    end
+  else
+    defp otp_cacerts, do: nil
   end
 end
