eea76ec3e619859b5f469c01d4e959c43c4b8071
Jon Ator <jonathanator0@gmail.com>
2025-04-16 20:16:42 -0400

    allow phx server

--- lib/flame/docker_backend.ex
+++ lib/flame/docker_backend.ex
@@ -118,7 +118,7 @@ defmodule FLAME.DockerBackend do
       |> FLAME.Parent.encode()
 
     new_env =
-      %{"PHX_SERVER" => "false", "FLAME_PARENT" => encoded_parent}
+      %{"FLAME_PARENT" => encoded_parent}
       |> Map.merge(state.env)
       |> then(fn env ->
         if flags = System.get_env("ERL_AFLAGS") do
