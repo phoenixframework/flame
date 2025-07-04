69856e1b334d5f69976fba805f58239c9c68296a
terrcin <terrcin@pheme.im>
2025-07-04 17:13:10 +1200

    updated FlyBackend to allow assigning existing vols to the machine on boot

--- lib/flame/fly_backend/mounts.ex
+++
@@ -1,9 +0,0 @@
-defmodule FLAME.FlyBackend.Mounts do
-  @derive Jason.Encoder
-  defstruct name: nil,
-            path: nil,
-            volume: nil,
-            extend_threshold_percent: 0,
-            add_size_gb: 0,
-            size_gb_limit: 0
-end
